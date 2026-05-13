96b7654937ab179c9009b5693e135e4420aa5672
Gerardo Palma <ingerardopalma@gmail.com>
2026-05-13 17:23:11 -0600

    Updates

--- assets/arrow-up-right-ZiaxWxlW.js
+++ assets/arrow-up-right-ZiaxWxlW.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-ZBa30Cpf.js";/**
+import{c as t}from"./index-COCSD8zd.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
