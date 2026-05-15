87483b2bd852bc55a644a5649bc1b1a02fbc79de
Gerardo Palma <ingerardopalma@gmail.com>
2026-05-15 15:31:59 -0600

    Updates

--- assets/arrow-up-right-BndWH6s6.js
+++ assets/arrow-up-right-BndWH6s6.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-Cf05YTxH.js";/**
+import{c as t}from"./index-mNY0GQZq.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/map-pin-CArLSUEW.js
+++ assets/map-pin-CArLSUEW.js
@@ -1,4 +1,4 @@
-import{c}from"./index-Cf05YTxH.js";/**
+import{c}from"./index-mNY0GQZq.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
