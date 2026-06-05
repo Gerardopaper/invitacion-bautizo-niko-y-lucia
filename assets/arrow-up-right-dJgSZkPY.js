61a2ef82de00418071502ac1849d9769b3e7627c
Gerardo Palma <gerardo.palma.perrusquia3@huawei.com>
2026-06-05 16:08:34 -0600

    Updates

--- assets/arrow-up-right-dJgSZkPY.js
+++ assets/arrow-up-right-dJgSZkPY.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-CzGCeqcp.js";/**
+import{c as t}from"./index-BGOGkC8H.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
